de2af781982e9481ea90d99f57b7bff971e09e14
RaphSku <rapsku.dev@gmail.com>
2023-10-02 15:53:23 +0200

    Added simple JWT authentication example with ECDSA + added docker compose file for postgres

--- auth/docker-entrypoint-initdb.d/init.sql
+++ auth/docker-entrypoint-initdb.d/init.sql
@@ -1,1 +1,21 @@
 CREATE DATABASE auth;
+
+CREATE TABLE login (
+    userid bigserial not null,
+    email varchar(200) not null,
+    username varchar(200) not null,
+    passwordhash bytea not null,
+    salt bytea not null,
+    unique (email),
+    unique (username),
+    constraint pk_userid primary key (userid)
+);
+
+CREATE TABLE login_activity (
+    activityid bigserial not null,
+    userid bigint not null,
+    username varchar(200) not null,
+    timestamp timestamp not null,
+    constraint pk_activityid primary key (activityid),
+    constraint fk_userid foreign key (userid) references login (userid)
+);
---
+++ auth/jwt_authentication/go.mod
@@ -0,0 +1,15 @@
+module github.com/RaphSku/RoadToDistributedSystems/tree/main/auth/jwt_authentication
+
+go 1.20
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/hashicorp/go-hclog v1.5.0
+)
+
+require (
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+)
