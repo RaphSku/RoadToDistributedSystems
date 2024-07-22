11b23a52047a97de213bb6dfae2ba47f72ed8ace
Raphael Skuza <rapsku.dev@gmail.com>
2024-07-22 13:58:49 +0200

    Enhancement: Cleanup of Makefiles and other configurations
    Signed-off-by: Raphael Skuza <rapsku.dev@gmail.com>

--- auth/docker-entrypoint-initdb.d/init.sql
+++ auth/docker-entrypoint-initdb.d/init.sql
@@ -1,5 +1,10 @@
+-- Create a database called auth
 CREATE DATABASE auth;
 
+-- Connect to database auth
+\c auth;
+
+-- Create a table called login that will be used in the authentication example
 CREATE TABLE login (
     userid bigserial not null,
     email varchar(200) not null,
